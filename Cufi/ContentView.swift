6801bee6bddaa680f13c8879663adaf22ef1aca8
Jonathan0827 <limjunehyeop@gmail.com>
2026-03-17 20:05:11 +0900

    Initial Commit

---
+++ Cufi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cufi
+//
+//  Created by Jonathan Lim on 3/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Cufi/CufiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CufiApp.swift
+//  Cufi
+//
+//  Created by Jonathan Lim on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct CufiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
